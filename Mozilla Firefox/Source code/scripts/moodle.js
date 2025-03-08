04f49866a9878bf67351ea7a2fcaf9b012a6f572
Guacamoleboy <twitch@galleripermoller.dk>
2025-03-08 22:26:54 +0100

    Link fix

---
+++ Mozilla Firefox/Source code/scripts/moodle.js
@@ -0,0 +1,6 @@
+var x = document.getElementsByTagName("a")
+for (i = 0; i < x.length; i++) {
+  if (x[i].getAttribute("href") == "https://dat1cphbusiness.github.io/content/Java_IntelliJ-File/") {
+    x[i].setAttribute("href", "https://dat1cphbusiness.github.io/content/Java_IntelliJ_File/")
+  }
+}
